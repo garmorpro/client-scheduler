eeccc24b336526c5b2aeefddf179a19120e0cbd1
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-06 15:25:45 -0500

    update

--- web/pages/master-schedule.php
+++ web/pages/master-schedule.php
@@ -221,7 +221,7 @@ function deleteAssignment(assignmentId) {
             </div>
             <div class="col-md-3 d-flex align-items-center gap-3">
                 <input type="date" name="start" class="form-control" value="<?php echo htmlspecialchars($startDate); ?>" onchange="autoSubmitDateFilter()">
-                <a href="?start=<?php echo date('Y-m-d', strtotime('monday -3 weeks')); ?>" class="btn btn-outline-secondary">Today</a>
+                <a href="?start=<?php echo date('Y-m-d', strtotime('monday -2 weeks')); ?>" class="btn btn-outline-secondary">Today</a>
             </div>
         </form>
     </div>
