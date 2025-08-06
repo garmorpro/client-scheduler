ecc2393077e6c510f787c8e791eaf2221aed7d94
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-06 16:37:00 -0500

    update

--- web/pages/master-schedule.php
+++ web/pages/master-schedule.php
@@ -283,7 +283,7 @@ function deleteAssignment(assignmentId) {
                     <tr>
                         <td class="text-start fw-semibold">
                             <?php echo htmlspecialchars($employees[$userId]['full_name']); ?>
-                            <span class="text-muted text-capitalize" style="font-size: 12px;">&nbsp;&nbsp;<?php echo $employees[$userId]['role']; ?></span>
+                            <span class="text-muted text-capitalize" style="font-size: 10px;">&nbsp;&nbsp;<?php echo $employees[$userId]['role']; ?></span>
                         </td>
 
                         <?php foreach ($mondays as $monday): ?>
