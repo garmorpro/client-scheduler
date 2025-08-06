5c507ffbac5d6508befde8d150243ac2c450d291
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-06 07:23:52 -0500

    update

--- web/index.php
+++ web/index.php
@@ -20,11 +20,11 @@
         <form class="p-4">
             <div class="mb-3">
                 <label for="email" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="email" placeholder="Enter email" required>
+                <input type="email" class="form-control" id="email" placeholder="Enter your email" required>
             </div>
             <div class="mb-3">
                 <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" id="password" placeholder="Password" required>
+                <input type="password" class="form-control" id="password" placeholder="Enter your password" required>
             </div>
             <div class="d-grid">
                 <button type="submit" class="btn" style="background-color: rgb(23,62, 70); color: white;">Login</button>
