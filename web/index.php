f2f2bcdf94c06e6effecfb305b743ee607ba248f
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-06 01:04:52 -0500

    update

--- web/index.php
+++ web/index.php
@@ -1,1 +1,13 @@
-Update here! making sure this is where all the code is going!
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body styles="background-color: rgb(247,249,251);">
+    
+
+
+</body>
+</html>
