ec3d5c88a6cdd7d006aae2b84265c6d9c93873d5
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-06 07:45:10 -0500

    update

--- web/includes/db.php
+++ web/includes/db.php
@@ -4,9 +4,13 @@ $db   = getenv('DB_NAME');
 $user = getenv('DB_USER');
 $pass = getenv('DB_PASSWORD');
 
+// Create a connection
 $conn = new mysqli($host, $user, $pass, $db);
 
+// Check if connection was successful
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Database connection successful!";
 }
 ?>
