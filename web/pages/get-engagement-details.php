0488a40b85fedb9f36c7cb19a42e60e7282d21cd
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-07 09:00:49 -0500

    udpate

--- web/pages/get-engagement-details.php
+++ web/pages/get-engagement-details.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 require_once '../includes/db.php';
-echo "DB included"; // Debugging line
+
 
 // Check if $conn (MySQLi) is defined
 if (!isset($conn)) {
