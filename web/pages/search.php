3f0b477bc859f2e77c4411a63c6285278f25c64f
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-07 07:54:13 -0500

    update

--- web/pages/search.php
+++ web/pages/search.php
@@ -5,7 +5,7 @@ if (isset($_GET['query'])) {
     $query = '%' . $_GET['query'] . '%';
 
     // Search for clients (engagements)
-    $clientQuery = "SELECT engagement_id AS id, client_name AS name, 'client' AS type FROM engagements WHERE client_name LIKE ? AND status = 'active'";
+    $clientQuery = "SELECT engagement_id AS id, client_name AS name, 'client' AS type FROM engagements WHERE client_name LIKE ?";
     $stmt = $conn->prepare($clientQuery);
     $stmt->bind_param('s', $query);
     $stmt->execute();
