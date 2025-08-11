4c08a73f920a38f9ae63ac6ebf9e4e656f83ed29
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-11 08:24:18 -0500

    update

--- web/pages/admin-panel.php
+++ web/pages/admin-panel.php
@@ -528,7 +528,7 @@ if (!isset($_SESSION['user_id'])) {
             <div class="row g-3 ps-3 pe-3 mt-2">
                 <div class="col-md-12">
                     <div class="analytic-card">
-                        <div class="analytic-header" style="font-size: 14px;">
+                        <div class="user-management-header" style="font-size: 14px;">
                         <div class="titles">
                             <p class="text-black"><strong>Advanced Reports</strong></p>
                             <p>Generate detailed system reports</p>
