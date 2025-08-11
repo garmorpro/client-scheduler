09fdf75f29d4a98fc87592d182f827e6313c2849
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-11 13:06:11 -0500

    update

--- web/pages/admin-panel.php
+++ web/pages/admin-panel.php
@@ -34,7 +34,7 @@ $result = mysqli_query($conn, $sql);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
 
-    <link rel="stylesheet" href="../assets/css/styles.css">
+    <link rel="stylesheet" href="../assets/css/styles.css?v=<?php echo time(); ?>">
 </head>
 <body class="d-flex">
 
