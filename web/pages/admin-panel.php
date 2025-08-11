b15d81f37e78d8f30742d42e7222d52a639b1789
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-10 23:21:54 -0500

    update

--- web/pages/admin-panel.php
+++ web/pages/admin-panel.php
@@ -282,7 +282,7 @@ if (!isset($_SESSION['user_id'])) {
                 </div>
                 <div class="user-management-buttons">
                     <a class="badge text-black p-2 text-decoration-none fw-medium" style="border: 1px solid rgb(229,229,229);"><i class="bi bi-upload me-3"></i>Import Users</a>
-                    <a class="badge text-black p-2 text-decoration-none fw-medium" style="background-color: rgb(3,2,18); color: white; border: 1px solid rgb(3,2,18);"><i class="bi bi-person-plus me-3"></i>Add User</a>
+                    <a class="badge text-white p-2 text-decoration-none fw-medium" style="background-color: rgb(3,2,18); border: 1px solid rgb(3,2,18);"><i class="bi bi-person-plus me-3"></i>Add User</a>
                 </div>
             </div>
 
