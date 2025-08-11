ea41ee810a34538baa08c80f02cf7ceff9d35f90
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-11 14:30:22 -0500

    update

--- web/pages/admin-panel.php
+++ web/pages/admin-panel.php
@@ -680,15 +680,15 @@ if ($result && $result->num_rows > 0) {
                     </h6>
                     <p class="text-muted">
                       <strong>Created:</strong>
-                      <span id="view_first_name_detail" class="d-inline-block text-end" style="min-width: 60%;"></span>
+                      <span id="view_acct_created" class="d-inline-block text-end" style="min-width: 60%;"></span>
                     </p>
                     <p class="text-muted">
                         <strong>Last Active:</strong> 
-                        <span id="view_last_name_detail" class="d-inline-block text-end" style="min-width: 60%;"></span>
+                        <span id="view_acct_last_active" class="d-inline-block text-end" style="min-width: 60%;"></span>
                     </p>
                     <p class="text-muted">
                         <strong>Status:</strong> 
-                        <span id="view" class="d-inline-block text-end" style="min-width: 75%;"></span>
+                        <span id="view_acct_status" class="d-inline-block text-end" style="min-width: 75%;"></span>
                     </p>
                 </div>
             </div>
@@ -945,7 +945,6 @@ if ($result && $result->num_rows > 0) {
       setText('view_email_detail', user.email);
 
       setText('view_status', user.status);
-      setText('view_acct_status', user.status);
       setText('view_acct_status', user.status);
       setText('view_acct_created', user.created);
       setText('view_acct_last_active', user.last_active);
