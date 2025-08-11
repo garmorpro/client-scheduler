5b9d7e6c4a5e0b236c4e397d265f9305b4b886f0
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-11 13:14:00 -0500

    update

--- web/pages/admin-panel.php
+++ web/pages/admin-panel.php
@@ -619,7 +619,7 @@ if ($result && $result->num_rows > 0) {
 
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn badge text-black p-2 text-decoration-none fw-medium overlay-red" style="font-size: .875rem; border: 1px solid rgb(229,229,229);" data-bs-dismiss="modal">Cancel</button>
+            <button type="button" class="btn badge text-black p-2 text-decoration-none fw-medium" style="font-size: .875rem; border: 1px solid rgb(229,229,229);" data-bs-dismiss="modal">Cancel</button>
             <button type="submit" class="badge text-white p-2 text-decoration-none fw-medium" style="font-size: .875rem; background-color: rgb(3,2,18);">Update User</button>
           </div>
         </form>
