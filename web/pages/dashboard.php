febd796e3638568deb58911ef45305641147a945
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-10 22:21:45 -0500

    update

--- web/pages/dashboard.php
+++ web/pages/dashboard.php
@@ -33,8 +33,8 @@ if (!isset($_SESSION['user_id'])) {
       margin: 0 auto 1rem auto;
     }
     .admin-icon img {
-      width: 24px;
-      height: 24px;
+      width: 28px;
+      height: 28px;
       color: #2c2c2c;
     }
     .admin-btn {
