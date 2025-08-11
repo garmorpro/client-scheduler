0d3bd1b79286b24567bc9ee61a7b68dbea25b6d5
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-11 14:25:45 -0500

    update

--- web/pages/admin-panel.php
+++ web/pages/admin-panel.php
@@ -658,10 +658,12 @@ if ($result && $result->num_rows > 0) {
 
             <div class="row">
                 <div class="col-md-6">
-                    <i class="bi bi-envelope"></i> Personal Information
+                    <h5>
+                        <i class="bi bi-envelope"></i> Personal Information
+                    </h5>
                     <p class="text-muted">
                       <strong>First Name:</strong>
-                      <span id="view_first_name_detail" class="d-inline-block text-end" style="min-width: 60%; font-size: 14px;"></span>
+                      <span id="view_first_name_detail" class="d-inline-block text-end" style="min-width: 60%;"></span>
                     </p>
                     <p class="text-muted">
                         <strong>Last Name:</strong> 
@@ -669,7 +671,7 @@ if ($result && $result->num_rows > 0) {
                     </p>
                     <p class="text-muted">
                         <strong>Email:</strong> 
-                        <span id="view_email_detail" class="d-inline-block text-end" style="min-width: 75%; font-size: 14px !important;"></span>
+                        <span id="view_email_detail" class="d-inline-block text-end" style="min-width: 75%;"></span>
                     </p>
                 </div>
                 <div class="col-md-6">
