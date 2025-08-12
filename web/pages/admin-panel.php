bfbc6820ec2deab5156551a901a4123d348e9ea4
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-12 00:55:49 -0500

    update

--- web/pages/admin-panel.php
+++ web/pages/admin-panel.php
@@ -1327,7 +1327,7 @@ if ($settingResult) {
 
 <!-- Backup Configuration Modal -->
     <div class="modal fade" id="backupConfigModal" tabindex="-1" aria-labelledby="backupConfigLabel" aria-hidden="true">
-      <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
+      <div class="modal-dialog modal-md modal-dialog-centered modal-dialog-scrollable">
         <div class="modal-content">
           <form id="backupConfigForm" action="settings_backend.php" method="POST">
             <div class="modal-header">
