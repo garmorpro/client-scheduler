a24801b2ca1e22b664ceea357590dec5576d8b5c
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-12 11:34:50 -0500

    update

--- web/pages/master-schedule.php
+++ web/pages/master-schedule.php
@@ -560,20 +560,21 @@ function openEmployeeModal(employeeId) {
 <?php if ($isAdmin): ?>
 
 <!-- Modal for Manage or Add Assignment -->
-  <div class="modal fade" id="manageAddModal" tabindex="-1">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title">Manage or Add Assignment</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          <button id="manageAssignmentsButton" class="btn btn-warning w-100 mb-2">Manage Existing Assignments</button>
-          <button id="addAssignmentsButton" class="btn btn-success w-100">Add New Assignment</button>
-        </div>
+  <div class="modal fade" id="manageAddModal" tabindex="-1" aria-hidden="true" aria-labelledby="manageAddModalLabel">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="manageAddModalLabel">Manage or Add Assignments</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <button id="manageAssignmentsButton" class="btn btn-warning w-100 mb-2">Manage Existing Assignments</button>
+        <button id="addAssignmentsButton" class="btn btn-success w-100">Add New Assignment</button>
       </div>
     </div>
   </div>
+</div>
+
 <!-- end manage or add assignment -->
 
 <!-- Modal for Managing Assignments -->
