f8f6e58357d71ba8855ffdf01a491abff1506c96
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-12 16:26:03 -0500

    update

--- web/pages/master-schedule.php
+++ web/pages/master-schedule.php
@@ -546,19 +546,34 @@ function openEmployeeModal(employeeId) {
 
 <!-- Modal for Manage or Add Assignment -->
   <div class="modal fade" id="manageAddModal" tabindex="-1">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title">Manage or Add Assignment</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Manage or Add Assignment</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+
+        <!-- Initial buttons -->
+        <div id="manageAddButtons">
           <button id="manageAssignmentsButton" class="btn btn-warning w-100 mb-2">Manage Existing Assignments</button>
           <button id="addAssignmentsButton" class="btn btn-success w-100">Add New Assignment</button>
         </div>
+
+        <!-- Assignments listing - hidden initially -->
+        <div id="assignmentsListing" class="d-none">
+          <button id="backToButtons" class="btn btn-secondary mb-3">Back</button>
+          <div id="assignmentsContainer">
+            <!-- Here you'll inject or render the list of assignments -->
+            <p>Loading assignments...</p>
+          </div>
+        </div>
+
       </div>
     </div>
   </div>
+</div>
+
 <!-- end manage or add assignment -->
 
 <!-- Modal for Managing Assignments -->
