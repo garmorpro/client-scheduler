f1959168023ea73007ab5cfb85ea7b73336d90e5
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-11 22:44:01 -0500

    update

--- web/pages/admin-panel.php
+++ web/pages/admin-panel.php
@@ -1209,7 +1209,8 @@ $engagementResults = mysqli_query($conn, $engagementSQL);
               <h6 class="mb-3">General Settings</h6>
               <div class="form-check form-switch mb-4">
                 <input class="form-check-input" type="checkbox" id="enableEmailNotifications" name="enable_email_notifications" checked>
-                <label class="form-check-label" for="enableEmailNotifications">Enable Email Notifications (Master switch)</label>
+                <label class="form-check-label" for="enableEmailNotifications">Enable Email Notifications (Master switch) <br><span>Master switch for all email notifications</span></label>
+
               </div>
 
               <!-- Notification Types -->
