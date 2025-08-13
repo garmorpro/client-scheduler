50ec52b11af25e5d9d3b679099e4462572f569fc
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-12 22:53:31 -0500

    udpate

--- web/includes/modals/manage_entries_prompt.php
+++ web/includes/modals/manage_entries_prompt.php
@@ -10,7 +10,7 @@
   <!-- Initial buttons -->
   <div id="entryTypePrompt" class="text-center">
     <p>Please choose the type of entry:</p>
-    <button id="manageEntriesButton" class="badge text-white p-2 text-decoration-none fw-medium" style="font-size: .875rem; background-color: rgb(3,2,18);">Manage Existing Entries</button>
+    <button id="manageEntriesButton" class="badge text-bg-warning text-white p-2 text-decoration-none fw-medium" style="font-size: .875rem; background-color: rgb(3,2,18);">Manage Existing Entries</button>
     <button id="addEntriesButton" class="badge text-white p-2 text-decoration-none fw-medium" style="font-size: .875rem; background-color: rgb(3,2,18);">Add New Entry</button>
   </div>
 
