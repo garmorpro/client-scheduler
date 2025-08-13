23d39b835b9df45ebd77bf39ad1e1872549f58c5
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-13 18:53:17 -0500

    update

--- web/pages/master-schedule.php
+++ web/pages/master-schedule.php
@@ -30,7 +30,22 @@ $rangeLabel = "Week of " . date('n/j', $firstWeek) . " - Week of " . date('n/j',
 
 // Get employees (keep same)
 $employees = [];
-$userQuery = "SELECT user_id, CONCAT(first_name, ' ', last_name) AS full_name, role FROM users WHERE status = 'active' AND role IN ('staff', 'senior')";
+$userQuery = "
+    SELECT 
+        user_id, 
+        CONCAT(first_name, ' ', last_name) AS full_name, 
+        role 
+    FROM users 
+    WHERE status = 'active' 
+      AND role IN ('staff', 'senior')
+    ORDER BY 
+        CASE 
+            WHEN role = 'senior' THEN 1
+            WHEN role = 'staff' THEN 2
+        END,
+        first_name ASC
+";
+
 $userResult = $conn->query($userQuery);
 if ($userResult) {
     while ($userRow = $userResult->fetch_assoc()) {
