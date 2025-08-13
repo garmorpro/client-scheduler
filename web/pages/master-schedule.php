01d39f9b2a486a84aa915d3d682e116a8c05ec9d
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-13 13:45:05 -0500

    update

--- web/pages/master-schedule.php
+++ web/pages/master-schedule.php
@@ -144,7 +144,7 @@ while ($D_row = $dropdownresult->fetch_assoc()) {
 
     <style>
       /* light highlight for time off cells */
-      .timeoff-cell { background-color: #d5d5d5ff !important; }
+      .timeoff-cell { background-color: #ebebebff !important; }
       .timeoff-corner { position: absolute; top: 2px; right: 6px; font-size: .50rem; }
     </style>
 </head>
