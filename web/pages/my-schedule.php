43bcad0a0bb7967d2391e2e61e40587c41c89346
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-12 23:39:44 -0500

    update

--- web/pages/my-schedule.php
+++ web/pages/my-schedule.php
@@ -120,7 +120,9 @@ foreach ($totalAssignedHours as $week => $hours) {
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <title>My Schedule</title>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/styles.css?v=<?php echo time(); ?>">
 <style>
 .highlight-today {
     background-color: #d3f9d8;
