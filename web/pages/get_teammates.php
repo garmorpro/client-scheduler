b96cefd849666d8fedbb6e4fc3066649d3a83d93
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-14 13:14:36 -0500

    update

--- web/pages/get_teammates.php
+++ web/pages/get_teammates.php
@@ -9,11 +9,14 @@ try {
     $weekStart     = $_GET['week_start'] ?? '';
     $clientName    = $_GET['client_name'] ?? '';
 
-    $sql = "SELECT u.user_id, u.first_name, u.last_name, u.user_name
-            FROM entries a
-            JOIN users u ON a.user_id = u.user_id
-            WHERE a.client_name = $clientName
-              AND a.week_start = $weekStart";
+    $sql = "SELECT 
+                e.engagement_id,
+                u.first_name,
+                u.last_name
+            FROM users u
+            JOIN entries e ON e.user_id = u.user_id
+            JOIN engagements g ON g.engagement_id = e.engagement_id
+            WHERE g.client_name = $clientName";
 
     if ($currentUserId) {
         $sql .= " AND u.id != :current_user_id";
