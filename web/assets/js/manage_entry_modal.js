d2ceae0a1c31e67c94cf42e295284309dec2074b
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-13 22:31:40 -0500

    update

--- web/assets/js/manage_entry_modal.js
+++ web/assets/js/manage_entry_modal.js
@@ -1,6 +1,12 @@
 function openManageEntryModal(user_id, employeeName, weekStart) {
-  const formattedDate = new Date(weekStart).toLocaleDateString('en-US', { month: 'short', day: 'numeric' });
-  document.getElementById('entriesModalTitle').innerText = `Manage Entires for Week of ${formattedDate}`;
+  // Convert weekStart to Date and add 1 day
+  const dateObj = new Date(weekStart);
+  dateObj.setDate(dateObj.getDate() + 1);
+
+  // Format the new date
+  const formattedDate = dateObj.toLocaleDateString('en-US', { month: 'short', day: 'numeric' });
+
+  document.getElementById('entriesModalTitle').innerText = `Manage Entries for Week of ${formattedDate}`;
   document.getElementById('entriesModalSubheading').innerText = `Consultant: ${employeeName}`;
 
   const entriesForWeek = entries[user_id] && entries[user_id][weekStart] ? entries[user_id][weekStart] : [];
