7cd97f31c77a19aa07a4d092181a4cf3788ce9fd
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-13 21:14:06 -0500

    update

--- web/pages/master-schedule.php
+++ web/pages/master-schedule.php
@@ -320,7 +320,7 @@ while ($D_row = $dropdownresult->fetch_assoc()) {
                                     onclick='
                                         event.stopPropagation();
                                         console.log("Entries empty?", false);
-                                        openAddEntryModal(
+                                        openManageEntryModal(
                                             "<?php echo $userId; ?>",
                                             <?php echo json_encode($fullName); ?>,
                                             "<?php echo $weekKey; ?>"
@@ -346,7 +346,7 @@ while ($D_row = $dropdownresult->fetch_assoc()) {
                                         openAddEntryModal(
                                             "<?php echo $userId; ?>",
                                             <?php echo json_encode($fullName); ?>,
-                                            "<?php echo '$weekKey'; ?>"
+                                            "<?php echo $weekKey; ?>"
                                         )
                                     '>
                                     <?php 
