b76a26b8c297306c62cf364ed196d9e730f219b8
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-13 20:03:04 -0500

    update

--- web/pages/storage_usage.php
+++ web/pages/storage_usage.php
@@ -1,21 +1,19 @@
 <?php
-$path = '/';
+$path = '/'; // Root filesystem for container/VM
 
 $totalSpace = disk_total_space($path);
 $freeSpace = disk_free_space($path);
 $usedSpace = $totalSpace - $freeSpace;
-
-// Get % used like df for non-root users
-$percentUsed = (($totalSpace - $freeSpace) / ($totalSpace - ($totalSpace*0.05))) * 100; // assume 5% reserved
+$percentUsed = ($usedSpace / $totalSpace) * 100;
 
 function formatSize($bytes) {
-    $sizes = ['B','KB','MB','GB','TB'];
+    $sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
     $i = 0;
-    while($bytes >= 1024 && $i < count($sizes)-1){
+    while ($bytes >= 1024 && $i < count($sizes) - 1) {
         $bytes /= 1024;
         $i++;
     }
-    return round($bytes,2) . ' ' . $sizes[$i];
+    return round($bytes, 2) . ' ' . $sizes[$i];
 }
 ?>
 <!DOCTYPE html>
@@ -26,7 +24,7 @@ function formatSize($bytes) {
 <body>
     <h1>Server Storage Usage</h1>
     <p>Total Space: <?= formatSize($totalSpace) ?></p>
-    <p>Used Space: <?= formatSize($usedSpace) ?> (<?= round($percentUsed,2) ?>%)</p>
+    <p>Used Space: <?= formatSize($usedSpace) ?> (<?= round($percentUsed, 2) ?>%)</p>
     <p>Free Space: <?= formatSize($freeSpace) ?></p>
 </body>
 </html>
