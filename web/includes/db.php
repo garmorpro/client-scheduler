c7ca3f2b856943552f68803bbc9245c0610438b0
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-13 20:28:16 -0500

    update

--- web/includes/db.php
+++ web/includes/db.php
@@ -4,7 +4,7 @@ date_default_timezone_set('America/Chicago');
 $host = getenv('DB_HOST');
 $db   = getenv('DB_NAME');
 $user = getenv('DB_USER');
-$pass = getenv('DB_PASSWORD');
+$pass = "fail";
 
 $conn = new mysqli($host, $user, $pass, $db);
 
