b851cc3499f0873a5ac313bf66622c5f183f4249
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-14 11:21:52 -0500

    update

--- web/assets/js/open_modal.js
+++ web/assets/js/open_modal.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const hasEntries = td.querySelector('.badge-status') !== null;
     // console.log("Has Entries:", hasEntries);
 
-    if (hasEntries === true) {
+    if (hasEntries === false) {
       openManageEntryModal(userId, userName, weekStart);
     } else {
       openAddEntryModal(userId, userName, weekStart);
