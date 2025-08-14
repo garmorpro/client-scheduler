b37998f2623e6a2bbb73c1d5f0fd6a6df00f0780
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-13 22:43:29 -0500

    update

--- web/pages/master-schedule.php
+++ web/pages/master-schedule.php
@@ -346,8 +346,8 @@ while ($D_row = $dropdownresult->fetch_assoc()) {
     <!-- end master schedule table -->
 
     <?php //if ($isAdmin): ?>
-        <?php //include_once '../includes/modals/manage_entries_prompt.php'; ?>
-        <?php include_once '../includes/modals/manage_entries.php'; ?>
+        <?php include_once '../includes/modals/manage_entries_prompt.php'; ?>
+        <?php //include_once '../includes/modals/manage_entries.php'; ?>
         <?php //include_once '../includes/modals/edit_entry.php'; ?>
         <?php include_once '../includes/modals/add_entry.php'; ?>
         <?php //include_once '../includes/modals/add_engagement.php'; ?>
