3105fd34e3075e0c2c7f35290b3073a1dafb0b83
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-14 22:38:43 -0500

    update

--- web/pages/master-schedule.php
+++ web/pages/master-schedule.php
@@ -225,7 +225,7 @@ table th:first-child,
 table td:first-child {
     position: sticky;
     left: 0;
-    z-index: 2 !important;
+    z-index: 1 !important;
     background: white;
     border: 1px solid blue;
 }
