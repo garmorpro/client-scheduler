cb907ed3d04fa1f227095b8e689052b88c10f125
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-15 16:18:45 -0500

    udpate

--- web/pages/master-schedule.php
+++ web/pages/master-schedule.php
@@ -130,7 +130,7 @@ $stmt2->close();
       .draggable-badge { cursor: grab; user-select: none; }
       .draggable-badge.dragging { opacity: 0.5; transform: scale(0.98); }
       td.drop-target { outline: 3px dashed rgba(0,123,255,0.15); }
-      td.addable:hover { background: rgba(0,0,0,0.02); }
+      /* td.addable:hover { background: rgba(0,0,0,0.02); } */
       th:first-child, td:first-child { min-width: 250px; position: sticky !important; left: 0; background-color: #fff; z-index: 101; outline: 2px solid rgb(223, 226, 230); border-left: 2px solid rgb(223, 226, 230); box-sizing: border-box; }
       .table-responsive { outline: 1px solid rgb(223, 226, 230); outline-offset: -1px; }
       .week { min-width: 200px; }
@@ -206,7 +206,7 @@ $stmt2->close();
                         $globalHours = $globalTimeOff[$weekKey] ?? null;
                         $isGlobalWeek = $globalHours !== null;
                         $isCurrentWeek = ($idx === $currentWeekIndex);
-                                            
+
                         // Combine classes
                         $thClasses = 'align-middle week';
                         if ($isGlobalWeek) $thClasses .= ' timeoff-cell';
