4c39ca48e05516d3e6cea4e8fce3c19289539ec5
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-14 21:30:24 -0500

    update

--- web/pages/master-schedule.php
+++ web/pages/master-schedule.php
@@ -217,7 +217,7 @@ while ($D_row = $dropdownresult->fetch_assoc()) {
     table th {
         position: sticky !important;
         top: 0px !important;
-        z-index: 3 !important;
+        z-index: 1 !important;
     }
     /* Sticky first column cells */
 /* Sticky first column */
