6bf44cf8c79b3c7c72c316b29abbf90d20ccad12
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-15 11:16:58 -0500

    update

--- web/includes/db.php
+++ web/includes/db.php
@@ -3,12 +3,12 @@ date_default_timezone_set('America/Chicago');
 
 // Get DB credentials from environment
 $host = getenv('DB_HOST');
-$db   = getenv('DB_NAME');
+$dbname   = getenv('DB_NAME');
 $user = getenv('DB_USER');
 $pass = getenv('DB_PASSWORD');
 
 // Turn off mysqli warnings (optional)
 mysqli_report(MYSQLI_REPORT_OFF);
 
-$db = new mysqli($host, $user, $pass, $db);
+$db = new mysqli($host, $user, $pass, $dbname);
 if ($db->connect_error) die("Connection failed: " . $db->connect_error);
