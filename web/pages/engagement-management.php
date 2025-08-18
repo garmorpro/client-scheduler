dd45bf336154f98eff9b599deec72e059d0a6fc8
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-18 16:15:46 -0500

    update

--- web/pages/engagement-management.php
+++ web/pages/engagement-management.php
@@ -127,7 +127,7 @@ $engagementResult = mysqli_query($conn, $engagementQuery);
         </div>
     <!-- end stats cards -->
 
-        <div class="flex-grow-1 p-4">
+        <div class="flex-grow-1">
             <div class="user-search" style="; max-width: 300px;">
                 <input type="text" id="engagementSearch" class="form-control form-control-sm" placeholder="Search engagements..." minlength="3">
             </div>
