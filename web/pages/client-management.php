2e8626c72441af4453b0c685e50cd6a9b3c8a7d2
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-18 12:35:26 -0500

    update

--- web/pages/client-management.php
+++ web/pages/client-management.php
@@ -137,8 +137,8 @@ $clients = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
 
             <!-- Card Buttons -->
             <div class="card-buttons d-flex flex-wrap gap-2">
-                <button class="badge text-black btn-sm fw-medium flex-grow-1 me-0" style="font-size: .875rem;"><i class="bi bi-eye"></i> View</button>
-                <button class="badge text-white btn-sm flex-grow-1 fw-medium border-none" style="font-size: .875rem;"><i class="bi bi-pencil"></i> Edit</button>
+                <button class="badge text-black btn-sm fw-medium flex-grow-1 me-0" style="font-size: .875rem; border: none !important;"><i class="bi bi-eye"></i> View</button>
+                <button class="badge text-white btn-sm flex-grow-1 fw-medium" style="font-size: .875rem; background-color: rgb(3,2,18); border: none !important;"><i class="bi bi-pencil"></i> Edit</button>
                 <!-- <button class="btn btn-outline-dark btn-sm flex-grow-1 me-0" style="border-color: rgb(229,229,229) !important;"><i class="bi bi-eye"></i> View</button>
                 <button class="btn btn-outline-secondary btn-sm flex-grow-1" style="background-color: rgb(229,229,229) !important;border-color: rgb(229,229,229) !important;"><i class="bi bi-pencil"></i> Edit</button> -->
             </div>
