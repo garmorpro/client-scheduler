38778f01f2f01838f18f8c38810693fa3721c209
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-18 20:54:15 -0500

    update

--- web/pages/master-schedule.php
+++ web/pages/master-schedule.php
@@ -362,7 +362,7 @@ while ($D_row = $dropdownresult->fetch_assoc()) {
   <div class="modal-dialog modal-dialog-centered modal-lg">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="employeeModalLabel">Employee Info</h5>
+        <!-- <h5 class="modal-title" id="employeeModalLabel">Employee Info</h5> -->
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
