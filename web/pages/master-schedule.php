1da9e400b0cbc591d92162f75b120348ff9cb0a2
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-18 20:54:51 -0500

    update

--- web/pages/master-schedule.php
+++ web/pages/master-schedule.php
@@ -365,7 +365,7 @@ while ($D_row = $dropdownresult->fetch_assoc()) {
         <!-- <h5 class="modal-title" id="employeeModalLabel">Employee Info</h5> -->
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <div class="modal-body">
+      <div class="modal-body" style="margin-top: -25px !important;">
         <div id="employeeModalContent">
           <!-- Dynamic content goes here -->
         </div>
