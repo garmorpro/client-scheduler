298dc960b9a7cb1f1589849803ad8fa7dc9c1eff
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-20 16:08:53 -0500

    update

--- web/pages/admin-panel.php
+++ web/pages/admin-panel.php
@@ -412,9 +412,6 @@ if ($result && mysqli_num_rows($result) > 0) {
                                             <td><?php echo date("n/j/Y", strtotime($entry['week_start'])); ?></td>
                                             <td><?php echo htmlspecialchars($entry['hours']); ?> hrs</td>
                                             <td class="table-actions">
-                                                <a href="#" class="edit-timeoff-btn text-decoration-none" data-bs-toggle="modal" data-bs-target="#editTimeoffModal" data-timeoff-id="<?php echo $entry['timeoff_id']; ?>">
-                                                    <i class="bi bi-pencil text-purple"></i>
-                                                </a>
                                                 <a href="#" class="delete-timeoff-btn text-decoration-none" data-timeoff-id="<?php echo $entry['timeoff_id']; ?>">
                                                     <i class="bi bi-trash text-danger"></i>
                                                 </a>
