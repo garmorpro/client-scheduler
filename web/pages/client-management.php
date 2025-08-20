9df6fab69b025c0c98b0b3b1ad39739498ede881
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-20 16:45:06 -0500

    update

--- web/pages/client-management.php
+++ web/pages/client-management.php
@@ -179,7 +179,7 @@ unset($client);
                     </button>
                 <div class="card-buttons d-flex flex-wrap gap-2">
                     
-                    <button class="badge text-black btn-sm fw-medium flex-grow-1 me-0 p-2" style="font-size: .875rem; border: none !important;"><i class="bi bi-eye"></i> View</button>
+                    <button class="badge text-black btn-sm fw-medium flex-grow-1 me-0 p-2" style="font-size: .875rem; outline: 1px solid rgb(229,229,229) !important;"><i class="bi bi-eye"></i> View</button>
                     <button class="badge text-white btn-sm flex-grow-1 fw-medium p-2" style="font-size: .875rem; background-color: rgb(3,2,18); border: none !important;"><i class="bi bi-pencil"></i> Edit</button>
                 </div>
             </div>
