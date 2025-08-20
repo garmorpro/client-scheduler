2e6e1ae167ae12b797a85ea54bf05eafa73a85f3
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-20 17:48:28 -0500

    update

--- web/pages/client-management.php
+++ web/pages/client-management.php
@@ -251,7 +251,7 @@ unset($client);
   <div class="modal-dialog modal-md modal-dialog-scrollable">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title">Client Details</h5>
+        <h5 class="modal-title"></h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body" id="viewClientModalBody">
