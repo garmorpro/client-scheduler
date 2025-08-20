ee3aa90ce69950bda250218b880b96b7a9edec99
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-20 08:16:59 -0500

    update

--- web/pages/master-schedule.php
+++ web/pages/master-schedule.php
@@ -367,8 +367,8 @@ while ($D_row = $dropdownresult->fetch_assoc()) {
                 <h5 class="modal-title" id="employeeModalLabel">Employee Info</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
-              <div class="modal-body" style="max-height: 70vh !important; overflow-y: auto !important;">
-                <div id="employeeModalContent" class="">
+              <div class="modal-body" style="max-height: 70vh !important;">
+                <div id="employeeModalContent" class=" overflow-y: auto !important;">
                   <!-- Dynamic content goes here -->
                 </div>
               </div>
