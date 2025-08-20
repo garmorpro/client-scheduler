670c1a28c327d85fcb9092016fb8a439789ce3f6
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-20 17:52:06 -0500

    update

--- web/pages/client-management.php
+++ web/pages/client-management.php
@@ -299,14 +299,14 @@ document.addEventListener('DOMContentLoaded', () => {
     <div style="background-color: rgb(245,245,247); border-radius: 15px; display: flex; align-items: center; gap: 10px; padding: 10px; margin-top: -20px;">
         <div class="justify-content-between d-flex" style="flex-grow: 1;">
             <div id="view_client_name" class="fw-semibold">${client.client_name}</div>
-            <small id="view_onboarded_date" class="text-muted">Onboarded: </small>
+            <small id="view_onboarded_date" class="text-muted">Onboarded<br>${new Date(client.onboarded_date).toLocaleDateString()}</small>
             
             
         </div>
     </div>
     <div class="d-flex gap-2 mt-2">
                 <div style="flex:1; background-color: white; border-radius: 10px; padding: 10px; text-align: center; box-shadow: 0 1px 3px rgba(0,0,0,0.1);">
-                    <div class="fw-semibold">${new Date(client.onboarded_date).toLocaleDateString()}</div>
+                    <div class="fw-semibold"></div>
                     <div class="text-muted" style="font-size: 12px;">Onboarded</div>
                 </div>
                 <div style="flex:1; background-color: white; border-radius: 10px; padding: 10px; text-align: center; box-shadow: 0 1px 3px rgba(0,0,0,0.1);">
