14cbb91159f41d6382ade6aaabbfb0eac88fa925
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-20 10:39:26 -0500

    update

--- web/api/login.php
+++ web/api/login.php
@@ -1,20 +1,25 @@
 <?php
 require_once '../api/api_helper.php';
+session_start();
 
-// Microsoft App settings
+// Generate code verifier and challenge
+$verifier = bin2hex(random_bytes(64));
+$_SESSION['code_verifier'] = $verifier;
+$challenge = rtrim(strtr(base64_encode(hash('sha256', $verifier, true)), '+/', '-_'), '=');
+
 $clientId = "d27315bd-3815-48d6-a27b-aeaa9fe2105a";
-$redirectUri = "https://10.10.254.127/api/callback.php"; // must match your app registration
+$redirectUri = "http://10.10.254.127/api/callback.php";
 $scopes = "openid profile email";
 
-// Build Microsoft login URL
 $authUrl = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize?" . http_build_query([
     "client_id" => $clientId,
     "response_type" => "code",
     "redirect_uri" => $redirectUri,
     "response_mode" => "query",
     "scope" => $scopes,
+    "code_challenge" => $challenge,
+    "code_challenge_method" => "S256"
 ]);
 
-// Redirect user to Microsoft login
 header("Location: $authUrl");
 exit;
