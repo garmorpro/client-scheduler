37c1fa92dae84c21030010ad7a68e92a0d384b4c
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-20 14:18:28 -0500

    update

--- web/auth/logout.php
+++ web/auth/logout.php
@@ -1,12 +1,19 @@
 <?php
 session_start();
-session_unset();
-session_destroy();
 
+// Set the message first
 $_SESSION['message'] = isset($_GET['timeout']) 
     ? "You have been logged out due to inactivity." 
     : "You have been logged out.";
 
+// Now clear other session data except message
+foreach ($_SESSION as $key => $value) {
+    if ($key !== 'message') {
+        unset($_SESSION[$key]);
+    }
+}
+
+// Redirect to login page
 header("Location: /");
 exit;
 ?>
