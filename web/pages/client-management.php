27d0f66d498aa8a56d99e5e7a09d96aca6b21d1c
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-20 17:53:34 -0500

    update

--- web/pages/client-management.php
+++ web/pages/client-management.php
@@ -296,7 +296,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
                 // Fill modal content with styled top details
                 let html = `
-    <div style="background-color: rgb(245,245,247); border-radius: 15px; display: flex; align-items: center; gap: 10px; padding: 10px; margin-top: -20px;">
+    <div class="align-content-center" style="background-color: rgb(245,245,247); border-radius: 15px; display: flex; align-items: center; gap: 10px; padding: 10px; margin-top: -20px;">
         <div class="justify-content-between d-flex" style="flex-grow: 1;">
             <div id="view_client_name" class="fw-semibold">${client.client_name}</div>
             <small id="view_onboarded_date" class="text-end">Onboarded<br><span class="text-muted">${new Date(client.onboarded_date).toLocaleDateString()}</span></small>
