253e5ffd4ac4a8e5c364abd1a05a27a60f110d4e
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-20 20:49:25 -0500

    update

--- web/pages/client-management.php
+++ web/pages/client-management.php
@@ -368,7 +368,7 @@ if (client.status.toLowerCase() === 'active') {
                     <div>
                       <div style="font-size: 10px;">
                         <span class="me-2"><span class="text-muted">Budgeted:</span> <span class="fw-bold text-black">${h.budgeted_hours}h</span></span>
-                        <span class="text-muted">Allocated:</span> <span class="fw-bold text-black">${h.allocated_hours}</span>
+                        <span class="text-muted">Allocated:</span> <span class="fw-bold text-black">${h.allocated_hours}h</span>
                       </div>
                     </div>
                 </div>
