c9a714ff3c8942e5a57ecd91d0caa0ddbbe1aaa5
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-08-20 22:26:21 -0500

    update

--- web/pages/client-management.php
+++ web/pages/client-management.php
@@ -248,17 +248,14 @@ unset($client);
 
 <!-- View Client Modal -->
 <div class="modal fade" id="viewClientModal" tabindex="-1" aria-hidden="true">
-  <div class="modal-dialog modal-md modal-dialog-scrollable">
+  <div class="modal-dialog modal-dialog-centered modal-lg">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title"></h5>
+        <h5 class="modal-title">Client Details</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body" id="viewClientModalBody">
-        <!-- Filled dynamically by JS -->
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <!-- content injected here -->
       </div>
     </div>
   </div>
