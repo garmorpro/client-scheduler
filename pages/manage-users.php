5fbe4068853896beaa8f7558de74c3d706558b03
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-02-27 14:44:27 -0600

    update

--- pages/manage-users.php
+++ pages/manage-users.php
@@ -265,6 +265,7 @@ $totalUsers = count($users);
     <script src="../assets/js/viewProfileModal.js?v=<?php echo time(); ?>"></script>
     <script src="../assets/js/openUpdateProfileDetailsModal.js?v=<?php echo time(); ?>"></script>
     <script src="../assets/js/theme_mode.js?v=<?php echo time(); ?>"></script>
+    <script src="../assets/js/import_users.js?v=<?php echo time(); ?>"></script>
 
     <script src="../assets/js/inactivity_counter.js?v=<?php echo time(); ?>"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
