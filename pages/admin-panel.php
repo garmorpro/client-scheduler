b86e2d75fa7d0220426c82d5c4a36bbeeae3e932
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-02-27 11:33:00 -0600

    update

--- pages/admin-panel.php
+++ pages/admin-panel.php
@@ -167,5 +167,6 @@ if ($settingResult) {
 </div> <!-- flex-grow -->
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="../assets/js/theme_mode.js?v=<?php echo time(); ?>"></script>
 </body>
 </html>
