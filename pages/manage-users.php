12d24191a84e23675958dd87c1c07bed76ed5845
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-02-27 16:17:04 -0600

    update

--- pages/manage-users.php
+++ pages/manage-users.php
@@ -33,7 +33,7 @@ $totalUsers = count($users);
 <html>
 <head>
     <title>Manage Users</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
     <link rel="stylesheet" href="../assets/css/styles.css?v=<?php echo time(); ?>">
 
