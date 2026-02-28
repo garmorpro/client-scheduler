b7014967724411c12b2c4b6c8d79a30655bd3490
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-02-28 11:08:49 -0600

    update

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -80,8 +80,8 @@ $currentPage = basename($_SERVER['PHP_SELF']);
         <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small ps-4">
             <li><a href="manage-users.php" class="nav-link text-dark py-1 <?= $currentPage == 'manage-users.php' ? 'active' : '' ?>">Users</a></li>
             <li><a href="company-holidays.php" class="nav-link text-dark py-1 <?= $currentPage == 'company-holidays.php' ? 'active' : '' ?>">Company Holidays</a></li>
-            <li><a href="notifications.php" class="nav-link text-dark py-1 <?= $currentPage == 'notifications.php' ? 'active' : '' ?>">Notifications</a></li>
-            <li><a href="reports.php" class="nav-link text-dark py-1 <?= $currentPage == 'reports.php' ? 'active' : '' ?>">Reports</a></li>
+            <!-- <li><a href="notifications.php" class="nav-link text-dark py-1 <?= $currentPage == 'notifications.php' ? 'active' : '' ?>">Notifications</a></li>
+            <li><a href="reports.php" class="nav-link text-dark py-1 <?= $currentPage == 'reports.php' ? 'active' : '' ?>">Reports</a></li> -->
         </ul>
     </div>
 </li>
